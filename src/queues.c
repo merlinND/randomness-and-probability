0413fc880bcef99df8cef7e6c7d29da1eb321979
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-03-24 16:02:38 +0100

    Mean number of clients + mean waiting time computation
    + indentation

--- src/queues.c
+++ src/queues.c
@@ -3,43 +3,42 @@
 #include <stdio.h>
 #include "distributions.h"
 
-queue_t mm1Queue(double lambda, double mu, double duration) {
-	// Init
-	double t, out = 0;
-	queue_t sim;
-	sim.arrivalsSize = 0;
-	sim.departuresSize = 0;
+queue_t mm1_queue(double lambda, double mu, double duration) {
+  // Init
+  double t = 0., out = 0.;
+  queue_t sim;
+  sim.arrivalsSize = 0;
+  sim.departuresSize = 0;
 
-	init_distributions();
+  init_distributions();
 
-	// Run the simulation in time
-	while (t < duration && sim.arrivalsSize < MAX_QUEUE_SIZE) {
-		t += next_exponential_value(lambda);
+  // Run the simulation in time
+  while (t < duration && sim.arrivalsSize < MAX_QUEUE_SIZE) {
+    t += next_exponential_value(lambda);
 
-		if (t < duration) {
-			sim.arrivals[sim.arrivalsSize] = t;
-			sim.arrivalsSize++;
+    if (t < duration) {
+      sim.arrivals[sim.arrivalsSize] = t;
+      sim.arrivalsSize++;
 
-			// This person arrived and could be served immediately
-			if (sim.arrivalsSize <= 1 || sim.arrivals[sim.arrivalsSize - 1] >= sim.departures[sim.arrivalsSize - 1]) {
-				out = t + next_exponential_value(mu);
-			}
-			// This person arrived but someone else was being served
-			else {
-				out = sim.departures[sim.departuresSize - 1] + next_exponential_value(mu);
-			}
-			if (out < duration) {
-				sim.departures[sim.departuresSize] = out;
-				sim.departuresSize++;
-			}
-		}
-	}
+      // This person arrived and could be served immediately
+      if (sim.arrivalsSize <= 1 || t >= sim.departures[sim.departuresSize - 1])
+        out = t + next_exponential_value(mu);
+      // This person arrived but someone else was being served
+      else
+        out = sim.departures[sim.departuresSize - 1] + next_exponential_value(mu);
 
-	return sim;
+      if (out < duration) {
+        sim.departures[sim.departuresSize] = out;
+        sim.departuresSize++;
+      }
+    }
+  }
+
+  return sim;
 }
 
-void queue_to_csv(queue_t q) {
-  printf("Timestamp,Number\n");
+// TODO: M/M/2 queue
+
 history_t make_history(queue_t const * q) {
   history_t history;
   history.size = 0;
@@ -65,5 +64,22 @@ history_t make_history(queue_t const * q) {
 
   return history;
 }
+
+double mean_clients_number(history_t const * h) {
+  // Weighted sum (a given number of client for a given time)
+  double sum = 0, dt;
+  for (int i = 1; i < h->size; ++i) {
+    dt = (h->points[i].t - h->points[i - 1].t);
+    sum += (dt * h->points[i - 1].n);
   }
+
+  double total = (h->points[h->size - 1].t - h->points[0].t);
+  return (sum / total);
+}
+double mean_waiting_time(queue_t const * q) {
+  double sum = 0;
+  for (int i = 0; i < q->departuresSize; ++i) {
+    sum += (q->departures[i] - (double)q->arrivals[i]);
+  }
+  return (sum / q->departuresSize);
 }
--- src/queues.h
+++ src/queues.h
@@ -4,16 +4,15 @@
 #define MAX_QUEUE_SIZE 1000
 
 struct queue_t {
-	int arrivalsSize;
-	// Times of arrivals
-	double arrivals[MAX_QUEUE_SIZE];
-	int departuresSize;
-	// Times of departures
-	double departures[MAX_QUEUE_SIZE];
+  int arrivalsSize;
+  // Times of arrivals
+  double arrivals[MAX_QUEUE_SIZE];
+  int departuresSize;
+  // Times of departures
+  double departures[MAX_QUEUE_SIZE];
 };
 typedef struct queue_t queue_t;
 
-queue_t mm1Queue(double lambda, double mu, double duration);
 struct data_point_t {
   double t;
   int n;
@@ -28,7 +27,8 @@ typedef struct history_t history_t;
 
 queue_t mm1_queue(double lambda, double mu, double duration);
 
-void queue_to_csv(queue_t q);
 history_t make_history(queue_t const * q);
+double mean_clients_number(history_t const * h);
+double mean_waiting_time(queue_t const * q);
 
 #endif
