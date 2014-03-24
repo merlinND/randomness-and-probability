23a0f0e1ca3ae67e2b9d2884e44ff69a1530f8de
Robin Ricard <ricard.robin@gmail.com>
2014-03-24 15:10:02 +0100

    Another case for MM1

--- src/mm1.c
+++ src/mm1.c
@@ -1,6 +1,15 @@
 #include "queues.h"
 
+#include <stdio.h>
+
 int main() {
+  printf(" - MM1 12 arrivals/hour, 20 departures/hour during 3 hours\n");
   queue_t q = mm1Queue(12/60., 20/60., 3*60);
+  queue_to_csv(q);
+
+  printf("\n\n");
+
+  printf(" - MM1 18 arrivals/hour, 20 departures/hour during 3 hours\n");
+  q = mm1Queue(18/60., 20/60., 3*60);
   queue_to_csv(q);
 }
