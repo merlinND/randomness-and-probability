56c2f00e9b98377668e9f8a8347a0e9051126d0a
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-03-24 14:27:15 +0100

    Simple "queue" data structure

--- src/queues.c
+++ src/queues.c
@@ -1,1 +1,2 @@
 #include "queues.h"
+
--- src/queues.h
+++ src/queues.h
@@ -1,4 +1,16 @@
 #ifndef QUEUES_H
 #define QUEUES_H
 
+#define MAX_QUEUE_SIZE 1000
+
+struct queue_t {
+	int arrivalsSize;
+	// Times of arrivals
+	double arrivals[MAX_QUEUE_SIZE];
+	int departuresSize;
+	// Times of departures
+	double departures[MAX_QUEUE_SIZE];
+};
+typedef queue_t queue_t;
+
 #endif
